2bfa0890762cb1faa21591bea85e305fc87944c6
Subigya <subigyapanta@gmail.com>
2017-12-05 15:15:50 +0700

    Added the one with softmax cross logit

--- mnist_beginner_with_softmax_cross.py
+++ mnist_beginner_with_softmax_cross.py
@@ -5,20 +5,23 @@ mnist = input_data.read_data_sets('MNIST_data/', one_hot = True)
 x = tf.placeholder(tf.float32, [None, 784]) # placeholder for x value in data
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-y = tf.nn.softmax(tf.matmul(x, W) + b) # holds estimated/predicted/probable digit in image
-# y = tf.nn.softmax_cross_entropy_with_logits(tf.matmul(x, W) + b) # same as above but more stable according to docs.
-
+# y = tf.nn.softmax(tf.matmul(x, W) + b) # holds estimated/predicted/probable digit in image
+# y = tf.nn.softmax_cross_entropy_with_logits(_sentinel=tf.matmul(x, W) + b) # same as above but more stable according to docs.
+y = tf.matmul(x, W) + b
 y_ = tf.placeholder(tf.float32, [None, 10]) # placeholder for y value in data
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
-
+count = True
 for _ in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
+  # if count:
+  #   # print('x:', batch_xs, 'y:', batch_ys)
+  #   count = False
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
   # print(type(batch_ys))
 
